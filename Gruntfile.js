60a1e7a54672d27edb6d94e8d5cf589f519bbec0
Doyle Young <dyoung@gmail.com>
2015-06-04 22:21:15 -0400

    Couple details, restyled in Idea

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,78 @@
-module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      all: ['Gruntfile.js', 'js/app.js']
-    },
-    clean: {
-      all: {
-        src: ['js/templates.js', 'minify/all.min.js', 'minify/all.min.css', '../dy_ninja.tar.gz'],
-        options: {
-          force: true
-        }
-      }
-    },
-    // tried multiple handlebars plugins, couldn't get them to wrap in an immediate
-    // function like command line executable
-    shell: {
-      options: {
-        stderr: false
-      },
-      target: {
-        command: '/usr/local/bin/handlebars templates/ -f js/templates.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
-        mangle: false,
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['Gruntfile.js', 'js/app.js']
+        },
+        clean: {
+            all: {
+                src: ['js/templates.js', 'minify/all.min.js', 'minify/all.min.css', '../dy_ninja.tar.gz'],
+                options: {
+                    force: true
+                }
+            }
+        },
+        // tried multiple handlebars plugins, couldn't get them to wrap in an immediate
+        // function like command line executable
+        shell: {
+            options: {
+                stderr: false
+            },
+            target: {
+                command: '/usr/local/bin/handlebars templates/ -f js/templates.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
+                mangle: false,
+                compress: {
+                    drop_console: true
+                }
+            },
+            my_target: {
+                files: {
+                    'minify/all.min.js': ['js/vendor/jquery.js',
+                        'js/foundation.min.js',
+                        'js/vendor/handlebars.runtime-v2.0.0.js',
+                        'js/templates.js',
+                        'js/app.js']
+                }
+            }
+        },
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+                },
+                files: {
+                    'minify/all.min.css': ['css/normalize.css',
+                        'css/foundation.css',
+                        'css/app.css']
+                }
+            }
+        },
         compress: {
-          drop_console: true
+            main: {
+                options: {
+                    archive: '../dy_ninja.tar.gz'
+                },
+                files: [
+                    {src: ['humans.txt', 'index.html', 'robots.txt'], dest: 'dy_ninja/'},
+                    {src: ['examples/*'], dest: 'dy_ninja/'},
+                    {src: ['img/*'], dest: 'dy_ninja/'},
+                    {src: ['js/vendor/modernizr.js'], dest: 'dy_ninja/'},
+                    {src: ['minify/*'], dest: 'dy_ninja/'}
+                ]
+            }
         }
-      },
-      my_target: {
-        files: {
-          'minify/all.min.js':
-            ['js/vendor/jquery.js',
-            'js/foundation.min.js',
-            'js/vendor/handlebars.runtime-v2.0.0.js',
-            'js/templates.js',
-            'js/app.js']
-        }
-      }
-    },
-    cssmin: {
-      add_banner: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
-        },
-        files: {
-          'minify/all.min.css':
-            ['css/normalize.css',
-            'css/foundation.css',
-            'css/app.css']
-        }
-      }
-    },
-    compress: {
-      main: {
-        options: {
-          archive: '../dy_ninja.tar.gz'
-        },
-        files: [
-          {src: ['humans.txt','index.html','robots.txt'], dest: 'dy_ninja/'},
-          {src: ['examples/*'], dest: 'dy_ninja/'},
-          {src: ['img/*'], dest: 'dy_ninja/'},
-          {src: ['minify/*'], dest: 'dy_ninja/'},
-        ]
-      }
-    }
-  });
+    })
+    ;
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['jshint','shell','uglify','cssmin']);
-};
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'shell', 'uglify', 'cssmin']);
+}
+;
